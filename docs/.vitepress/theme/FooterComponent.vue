cf1d256789823dcc3d12eba2d96d1844919ee7f6
trudog <pm.marek@pm.me>
2025-02-13 11:53:00 -0500

    resolved build errors

--- docs/.vitepress/theme/FooterComponent.vue
+++ docs/.vitepress/theme/FooterComponent.vue
@@ -10,8 +10,8 @@ const footer = frontmatter.value.footer;
 <template>
   <footer class="py-8 px-6 text-center text-sm text-gray-500  dark:text-gray-400 z-10 border-t border-neutral-200 dark:border-neutral-700">
     <div class="container mx-auto">
-      <p class="message">{{ footer.message }}</p>
-      <p class="copyright">{{ footer.copyright }}</p>
+      <p class="message">{{ footer?.message }}</p>
+      <p class="copyright">{{ footer?.copyright }}</p>
     </div>
   </footer>
 </template>
--- docs/.vitepress/theme/HeaderComponent.vue
+++ docs/.vitepress/theme/HeaderComponent.vue
@@ -6,7 +6,7 @@ const { frontmatter, isDark } = useData();
 
 const logo = frontmatter.value.logo;
 const addonName = frontmatter.value.addonName;
-const imgHeight = `height: ${logo.height}`
+const imgHeight = `height: ${logo?.height}`
 const toggleDarkMode = () => {
   isDark.value = !isDark.value;
 };
@@ -18,8 +18,8 @@ const toggleDarkMode = () => {
   <header :class="isDark ? 'bg-neutral-800': 'bg-neutral-200'" class="fixed top-0 left-0 w-full  shadow-md z-50">
     <div class="m-5 flex flex-nowrap items-center ">
       <div class=" flex flex-nowrap items-center ">
-        <img v-if="!isDark" :style="imgHeight" :src="logo.light" alt="Light Logo">
-        <img v-else :style="imgHeight" :src="logo.dark" alt="Dark Logo">
+        <img v-if="!isDark" :style="imgHeight" :src="logo?.light" alt="Light Logo">
+        <img v-else :style="imgHeight" :src="logo?.dark" alt="Dark Logo">
         <span class="font-bold ml-2">{{ addonName }}</span>
       </div>
       <button @click="toggleDarkMode" class="ml-auto text-sm p-2 bg-blue-500 dark:bg-blue-700 text-white rounded flex items-center">
